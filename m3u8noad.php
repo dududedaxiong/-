690d8776d93cfe5a5b3b70d4c6df6061ee5d8f8d
dududedaxiong <764840990@qq.com>
2025-11-28 15:37:18 +0800

    Add files via upload

--- m3u8noad.php
+++ m3u8noad.php
@@ -1,366 +1,177 @@
 [
- {
-  "hosts":[
-   "vip.lz",
-   "hd.lz",
-   "v.cdnlz",
-   "hd.ffzy",
-   "cdn-vipvideo",
-   "nbfjhertyrxiang",
-   "sekihfde",
-   "super.ffzy",
-   "fhbbff",
-   "vvip"
-  ],
-  "name":"量非",
-  "regex":[
-   "25.1",
-   "18.266",
-   "18.5333",
-   "23.466666",
-   "17.99",
-   "14.45",
-   "17.19",
-   "#EXTINF.*?\\s+.*?1o.*?\\.ts\\s+",
-   "#EXTINF.*?\\s+.*?11697777.*?\\.ts\\s+",
-   "#EXTINF.*?\\s+.*?p1ayer.*?\\.ts\\s+",
-   "#EXTINF.*?\\s+.*?1171(057).*?\\.ts",
-   "#EXTINF.*?\\s+.*?6d7b(077).*?\\.ts",
-   "#EXTINF.*?\\s+.*?6718a(403).*?\\.ts",
-   "#EXTINF.*?\\s+.*?adjump.*?\\.ts\\s+",
-   "#EXTINF.*?\\s+.*a241.*?\\.ts\\s+",
-   "#EXT-X-DISCONTINUITY\r*\n*#EXTINF:6.666667,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-DISCONTINUITY\r*\n*#EXTINF:6.600000,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXTINF.*?\\s+.*?1o.*?\\.ts\\s+",
-   "#EXTINF.*?\\s+.*?11697777.*?\\.ts\\s+",
-   "#EXTINF.*?\\s+.*?p1ayer.*?\\.ts\\s+",
-   "#EXT-X-DISCONTINUITY\r*\n*#EXTINF:7\\.166667,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-DISCONTINUITY\r*\n*#EXTINF:4\\.066667,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-DISCONTINUITY\r*\n*#EXTINF:6\\.400000,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:1\\.28,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXTINF.*?\\s+.*?1171(057).*?\\.ts",
-   "#EXTINF.*?\\s+.*?6d7b(077).*?\\.ts",
-   "#EXTINF.*?\\s+.*?6718a(403).*?\\.ts",
-   "#EXT-X-PLAYLIST-TYPE:VOD\\r*\\n*#EXTINF:5\\.600000,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY"
-  ]
- },
-
- {
-  "hosts":[
-   "haiwaikan"
-  ],
-  "name":"海外看",
-  "regex":[
-   "8.16",
-   "8.1748",
-   "10.0099",
-   "10.3333",
-   "10.85",
-   "12.33",
-   "16.0599",
-   "10.0099"
-  ]
- },
- {
-  "hosts":[
-   "suonizy"
-  ],
-  "name":"索尼",
-  "regex":[
-   "15.1666",
-   "15.2666",
-   "#EXT-X-DISCONTINUITY\r*\n*#EXTINF:7\\680000,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXTINF.*?\\s+.*?1o.*?\\.ts\\s+"
-  ]
- },
- {
-  "hosts":[
-   "vip.ffzy"
-  ],
-  "name":"ffzy",
-  "regex":[
-   "#EXT-X-DISCONTINUITY\r*\n*#EXTINF:.*?6667,[\\s\\S]*?#EXT-X-DISCONTINUITY"
-
-  ]
- },
   {
-  "hosts":[
-   "bwzm3u8.com"
-  ],
-  "name":"bwzm3u8",
-  "regex":[
-   "#EXT-X-KEY:METHOD=AES-128\r*\n*[\\s\\S]*?#EXTINF:1.28,[\\s\\S]*?#EXT-X-DISCONTINUITY"
-
-  ]
- },
+    "hosts": ["vip.lz","hd.lz","v.cdnlz","hd.ffzy","cdn-vipvideo","nbfjhertyrxiang","sekihfde","super.ffzy","fhbbff","vvip"],
+    "name": "量非",
+    "regex": [
+      "25.1","18.266","18.5333","23.466666","17.99","14.45","17.19",
+      "#EXTINF.*?\\s+.*?1o.*?\\.ts\\s+",
+      "#EXTINF.*?\\s+.*?11697777.*?\\.ts\\s+",
+      "#EXTINF.*?\\s+.*?p1ayer.*?\\.ts\\s+",
+      "#EXTINF.*?\\s+.*?1171(057).*?\\.ts",
+      "#EXTINF.*?\\s+.*?6d7b(077).*?\\.ts",
+      "#EXTINF.*?\\s+.*?6718a(403).*?\\.ts",
+      "#EXTINF.*?\\s+.*?adjump.*?\\.ts\\s+",
+      "#EXTINF.*?\\s+.*a241.*?\\.ts\\s+",
+      "#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:6\\.666667,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:6\\.600000,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:7\\.166667,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:4\\.066667,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:6\\.400000,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:1\\.28,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-PLAYLIST-TYPE:VOD\\r*\\n*#EXTINF:5\\.600000,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY"
+    ]
+  },
   {
-  "hosts":[
-   "xibaom"
-  ],
-  "name":"xibaom",
-  "regex":[
-   "#EXTINF:[\\s\\S]*?\/ad.*?\\.ts\n#EXT-X-DISCONTINUITY"
-
-  ]
- },
- {
-  "hosts":[
-   "bfengbf.com",
-   "bfzy",
-   "ddbbffcdn",
-   "rrcdnbf",
-  
-   "7bbffvip",
-   "bvvvvvvv7f"
-  ],
-  "name":"暴風",
-  "regex":[
-   "#EXTINF.*?\\s+.*?adjump.*?\\.ts\\s+",
-   "#EXT-X-DISCONTINUITY\r*\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXTINF.*?\\s+.*?1o.*?\\.ts\\s+"
-  ]
- },
- {
-  "hosts":[
-  
-   "97img",
-   "vvip",
-   "thm3u8",
-   "askzybf"
-  ],
-  "name":"去你的广告",
-  "regex":[
-  "#EXT-X-KEY:METHOD=AES-128,URI=.*?700k[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-  "#EXTINF:5,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-  "#EXT-X-KEY:METHOD=AES-128,URI=.*?\r*\n*#EXTINF:3\\.333,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=AES-128,URI=.*?\r*\n*#EXTINF:3.333,[\\s\\S]*\\.ts"
-   ]
- },
-
+    "hosts": ["haiwaikan"],
+    "name": "海外看",
+    "regex": ["8.16","8.1748","10.0099","10.3333","10.85","12.33","16.0599","10.0099"]
+  },
   {
-  "hosts":[
-   "aws.ulivetv.net"
-  ],
-  "name":"星星",
-  "regex":[
-   "#EXT-X-DISCONTINUITY\r*\n*#EXTINF:8,[\\s\\S]*?#EXT-X-DISCONTINUITY"
-  ]
- },
-   {
-  "hosts":[
-   "mzm3u8"
-  ],
-  "name":"mozhua",
-  "regex":[
-   "#EXT-X-DISCONTINUITY[\\s\\S]*?#EXT-X-KEY:METHOD=AES-128[\\s\\S]*?#EXT-X-DISCONTINUITY[\\s\\S]*?0x00000000000000000000000000000000"
-  ]
- },
-   {
-  "hosts":[
-   "ryplay",
-   "sysybf"
-  ],
-  "name":"猎手短剧",
-  "regex":[
-   "#EXT-X-DISCONTINUITY\r*\n*#EXTINF:5.680000,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-    "#EXTINF:4.4,[\\s\\S]*?#EXT-X-DISCONTINUITY"
-  ]
- },
- 
- 
-   {
-  "hosts":[
-   "modujx"
-  ],
-  "name":"魔都短剧",
-  "regex":[
-   "#EXTINF:3.333[\\s\\S]*?#EXTINF:3.2[\\s\\S]*?ts[\\s\\S]*?#EXT-X-DISCONTINUITY"
-  ]
- },
-   {
-  "hosts":[
-   "lzc"
-  ],
-  "name":"Lz采集",
-  "regex":[
-   "#EXTINF:6.66[\\s\\S]*?#EXT-X-DISCONTINUITY"
-  ]
- },
-
+    "hosts": ["suonizy"],
+    "name": "索尼",
+    "regex": ["15.1666","15.2666","#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:7\\\\680000,[\\s\\S]*?#EXT-X-DISCONTINUITY","#EXTINF.*?\\s+.*?1o.*?\\.ts\\s+"]
+  },
   {
-  "hosts":[
-   "fqm3u8"
-  ],
-  "name":"fq去插",
-  "regex":[
-   "#EXT-X-KEY:METHOD=AES-128[\\s\\S]*?#EXTINF:3[\\s\\S]*GcS4RzMZ[\\s\\S]*?#EXT-X-DISCONTINUITY"
-   
-  ]
-  
-
- },
-   {
-  "hosts":[
-   "ffzy-play"
-  ],
-  "name":"ffzy",
-  "regex":[
-   "#EXTINF.*?66667[\\s\\S]*?ts"
-   
-  ]
-  
-
- },
-    {
-  "hosts":[
-   "seu6tu",
-   "535fs",
-   "dzlndygh",
-   "hdvjl"
-  ],
-  "name":"熊猫视频",
-  "regex":[
-   "#EXTINF:[\\s\\S]*?encrypt0005.ts"
-   
-  ]
-  
-
- },
-     {
-  "hosts":[
-   "phncdn"
-  ],
-  "name":"pronhub",
-  "regex":[
-   "seg-1-v1-a1.ts[\\s\\S]*?seg-2-v1-a1.ts[\\s\\S]*?#EXTINF:.*?,"
-   
-  ]
-  
-
- },
-      {
-  "hosts":[
-   "aiwuci"
-  ],
-  "name":"aiwuci",
-  "regex":[
-   "#EXTINF:4.033[\\s\\S]*?.ts",
-   "#EXTINF:3[\\s\\S]*?W9K.ts"
-   
-  ]
-  
-
- },
-  
-   {
-  "hosts":[
-   "55p"
-  ],
-  "name":"55p去插",
-  "regex":[
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:4.4,[\\s\\S]*?#EXT-X-DISCONTINUITY"
-   
-  ]
-  
-
- },
-    {
-  "hosts":[
-   "bwzyplay.com"
-  ],
-  "name":"bwzyplay",
-  "regex":[
-   "#EXTINF:[\\s\\S]*?#EXT-X-DISCONTINUITY"
-   
-  ]
-  
-
- },
-     {
-  "hosts":[
-   "yutubf"
-  ],
-  "name":"yutubf",
-  "regex":[
-     "#EXT-X-KEY:METHOD=NONE\r*\n*[\\s\\S]*?#EXT-X-DISCONTINUITY",
-  ]
-  
-
- },
-    {
-  "hosts":[
-   "jkunnzyx",
-    "bbffvip",
-    "lbbf",
-    "bfbfhao",
-    "ddyunbo",
-    "bfaskbwerve",
-    "lsbbf",
-    "lbv3lb",
-    "sybf",
-    "qrtuv",
-    "vostrely",
-    "thm3u8.vip",
-    "newljlj.com",
-    ".slslvip",
-    "jpxm3u8"
-  ],
-  "name":"jkunnzyx",
-  
-  "regex":[
-  "#EXT-X-KEY:METHOD=AES-128,URI=.*?\r*\n*#EXTINF:3\\.333,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=AES-128,URI=.*?\r*\n*#EXTINF:3.333,[\\s\\S]*\\.ts",
-     "#EXT-X-KEY:METHOD=AES-128,URI=.*?\r*\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=AES-128,URI=.*?\r*\n*#EXTINF:3,[\\s\\S]*\\.ts",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:3,.*?ts",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:2,[\\s\\S]*?#EXT-X-DISCONTINUITY"
-   
-   
-  ]
-  
-
- },
- {
-  "hosts":[
-   "askzycdn",
-   "jkunbf",
-   "bfikuncdn",
-   "ddcdnbf",
-   "bfaskcdn"   
-  ],
-  "name":"开源棋牌",
-  "regex":[
-   "#EXTINF.*?\\s+.*?aaaa.*?\\.ts\\s+",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:5,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=AES-128,URI=\"[^\"]+\"\r*\n*#EXTINF:3\\.333,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:5,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:2\\.4,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:1\\.467,[\\s\\S]*?#EXT-X-DISCONTINUITY"
-  ]
- },
- {
-  "hosts":[
-   "nbfjhertyrxiang"
-  ],
-  "name":"LBW",
-  "regex":[
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:2\\.4,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:1\\.467,[\\s\\S]*?#EXT-X-DISCONTINUITY"
-  ]
- },
+    "hosts": ["vip.ffzy"],
+    "name": "ffzy",
+    "regex": ["#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:.*?6667,[\\s\\S]*?#EXT-X-DISCONTINUITY"]
+  },
   {
-  "hosts":[
-   "kuaikan"
-  ],
-  "name":"快看",
-  "regex":[
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:5,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:2\\.4,[\\s\\S]*?#EXT-X-DISCONTINUITY",
-   "#EXT-X-KEY:METHOD=NONE\r*\n*#EXTINF:1\\.467,[\\s\\S]*?#EXT-X-DISCONTINUITY"
-  ]
- }
+    "hosts": ["bwzm3u8.com"],
+    "name": "bwzm3u8",
+    "regex": ["#EXT-X-KEY:METHOD=AES-128\\r*\\n*[\\s\\S]*?#EXTINF:1.28,[\\s\\S]*?#EXT-X-DISCONTINUITY"]
+  },
+  {
+    "hosts": ["xibaom","xibaom3","xibaom3.top"],
+    "name": "香蕉视频·终极核平",
+    "regex": [
+      "/#EXT-X-DISCONTINUITY[\\s\\S]*?ad00[\\s\\S]*?#EXT-X-DISCONTINUITY/im"
+    ]
+  },
+  {
+    "hosts": ["bfengbf.com","bfzy","ddbbffcdn","rrcdnbf","7bbffvip","bvvvvvvv7f"],
+    "name": "暴風",
+    "regex": ["#EXTINF.*?\\s+.*?adjump.*?\\.ts\\s+","#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY","#EXTINF.*?\\s+.*?1o.*?\\.ts\\s+"]
+  },
+  {
+    "hosts": ["97img","vvip","thm3u8","askzybf"],
+    "name": "去你的广告",
+    "regex": [
+      "#EXT-X-KEY:METHOD=AES-128,URI=.*?700k[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXTINF:5,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=AES-128,URI=.*?\\r*\\n*#EXTINF:3\\.333,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=AES-128,URI=.*?\\r*\\n*#EXTINF:3\\.333,[\\s\\S]*\\.ts"
+    ]
+  },
+  {
+    "hosts": ["aws.ulivetv.net"],
+    "name": "星星",
+    "regex": ["#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:8,[\\s\\S]*?#EXT-X-DISCONTINUITY"]
+  },
+  {
+    "hosts": ["mzm3u8"],
+    "name": "mozhua",
+    "regex": ["#EXT-X-DISCONTINUITY[\\s\\S]*?#EXT-X-KEY:METHOD=AES-128[\\s\\S]*?#EXT-X-DISCONTINUITY[\\s\\S]*?0x00000000000000000000000000000000"]
+  },
+  {
+    "hosts": ["ryplay","sysybf"],
+    "name": "猎手短剧",
+    "regex": ["#EXT-X-DISCONTINUITY\\r*\\n*#EXTINF:5\\.680000,[\\s\\S]*?#EXT-X-DISCONTINUITY","#EXTINF:4.4,[\\s\\S]*?#EXT-X-DISCONTINUITY"]
+  },
+  {
+    "hosts": ["modujx"],
+    "name": "魔都短剧",
+    "regex": ["#EXTINF:3\\.333[\\s\\S]*?#EXTINF:3\\.2[\\s\\S]*?ts[\\s\\S]*?#EXT-X-DISCONTINUITY"]
+  },
+  {
+    "hosts": ["lzc"],
+    "name": "Lz采集",
+    "regex": ["#EXTINF:6\\.66[\\s\\S]*?#EXT-X-DISCONTINUITY"]
+  },
+  {
+    "hosts": ["fqm3u8"],
+    "name": "fq去插",
+    "regex": ["#EXT-X-KEY:METHOD=AES-128[\\s\\S]*?#EXTINF:3[\\s\\S]*GcS4RzMZ[\\s\\S]*?#EXT-X-DISCONTINUITY"]
+  },
+  {
+    "hosts": ["ffzy-play"],
+    "name": "ffzy",
+    "regex": ["#EXTINF.*?66667[\\s\\S]*?ts"]
+  },
+  {
+    "hosts": ["seu6tu","535fs","dzlndygh","hdvjl"],
+    "name": "熊猫视频",
+    "regex": ["#EXTINF:[\\s\\S]*?encrypt0005\\.ts"]
+  },
+  {
+    "hosts": ["phncdn"],
+    "name": "pronhub",
+    "regex": ["seg-1-v1-a1\\.ts[\\s\\S]*?seg-2-v1-a1\\.ts[\\s\\S]*?#EXTINF:.*?,"]
+  },
+  {
+    "hosts": ["aiwuci"],
+    "name": "aiwuci",
+    "regex": ["#EXTINF:4\\.033[\\s\\S]*?\\.ts","#EXTINF:3[\\s\\S]*?W9K\\.ts"]
+  },
+  {
+    "hosts": ["55p"],
+    "name": "55p去插",
+    "regex": ["#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:4\\.4,[\\s\\S]*?#EXT-X-DISCONTINUITY"]
+  },
+  {
+    "hosts": ["bwzyplay.com"],
+    "name": "bwzyplay",
+    "regex": ["#EXTINF:[\\s\\S]*?#EXT-X-DISCONTINUITY"]
+  },
+  {
+    "hosts": ["yutubf"],
+    "name": "yutubf",
+    "regex": ["#EXT-X-KEY:METHOD=NONE\\r*\\n*[\\s\\S]*?#EXT-X-DISCONTINUITY"]
+  },
+  {
+    "hosts": ["jkunnzyx","bbffvip","lbbf","bfbfhao","ddyunbo","bfaskbwerve","lsbbf","lbv3lb","sybf","qrtuv","vostrely","thm3u8.vip","newljlj.com",".slslvip","jpxm3u8"],
+    "name": "jkunnzyx",
+    "regex": [
+      "#EXT-X-KEY:METHOD=AES-128,URI=.*?\\r*\\n*#EXTINF:3\\.333,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=AES-128,URI=.*?\\r*\\n*#EXTINF:3\\.333,[\\s\\S]*\\.ts",
+      "#EXT-X-KEY:METHOD=AES-128,URI=.*?\\r*\\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=AES-128,URI=.*?\\r*\\n*#EXTINF:3,[\\s\\S]*\\.ts",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:3,.*?ts",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:2,[\\s\\S]*?#EXT-X-DISCONTINUITY"
+    ]
+  },
+  {
+    "hosts": ["askzycdn","jkunbf","bfikuncdn","ddcdnbf","bfaskcdn"],
+    "name": "开源棋牌",
+    "regex": [
+      "#EXTINF.*?\\s+.*?aaaa.*?\\.ts\\s+",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:5,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=AES-128,URI=\"[^\"]+\"\\r*\\n*#EXTINF:3\\.333,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:5,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:2\\.4,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:1\\.467,[\\s\\S]*?#EXT-X-DISCONTINUITY"
+    ]
+  },
+  {
+    "hosts": ["nbfjhertyrxiang"],
+    "name": "LBW",
+    "regex": [
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:3,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:2\\.4,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:1\\.467,[\\s\\S]*?#EXT-X-DISCONTINUITY"
+    ]
+  },
+  {
+    "hosts": ["kuaikan"],
+    "name": "快看",
+    "regex": [
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:5,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:2\\.4,[\\s\\S]*?#EXT-X-DISCONTINUITY",
+      "#EXT-X-KEY:METHOD=NONE\\r*\\n*#EXTINF:1\\.467,[\\s\\S]*?#EXT-X-DISCONTINUITY"
+    ]
+  }
 ]
+
