3606209543f55da1d8d2cf576064540f8f4aff8a
dududedaxiong <764840990@qq.com>
2025-11-30 13:36:01 +0800

    �� Auto sync ad blocking rules

--- js-ad-rules.js
+++ js-ad-rules.js
@@ -1,8 +1,75 @@
 {
     "*": {
+        "keywords": [
+            {
+                "selector": "li>a>font",
+                "keywords": [
+                    "澳门新葡京"
+                ]
+            }
+        ],
+        "classes": [
+            "ad",
+            "mda",
+            "notice",
+            "uni-swiper-slides"
+        ],
+        "selectors": [
+            "ads",
+            "div[style=\"23356\"]",
+            "div[style=\"opacity: 1;\"]",
+            "div[style=\"z-index: 2001;\"]",
+            "i.icon.iconfont.icon-account",
+            "iframe[style=\"position: absolute; display: block; left: 50%; top: 50%; width: 300px; height: 146px; margin: -73px 0px 0px -150px;\"]",
+            "uni-view.tabbar-list"
+        ],
+        "idsToHide": [
+            "__clb-spot_2090934_fxi_1",
+            "__clb-spot_2090934_mzi_1",
+            "rm-float3",
+            "啊路"
+        ],
+        "interval": 600
+    },
+    "cn2.91short.com": {
+        "keywords": [
+            {
+                "selector": "a>span.van-tab__text",
+                "keywords": [
+                    "一键脱衣",
+                    "催情春药",
+                    "同城约炮",
+                    "推荐"
+                ]
+            }
+        ],
+        "classes": [
+            "mda",
+            "notice"
+        ],
+        "selectors": [
+            "div[style=\"23356\"]",
+            "span[style=\"color: gold\"]"
+        ],
+        "idsToHide": [],
+        "interval": 600
+    },
+    "tktube.com": {
         "keywords": [],
-        "classes": [],
-        "selectors": [],
-        "idsToHide": []
+        "classes": [
+            "sponsor"
+        ],
+        "selectors": [
+            "iframe#stripchatHeader",
+            "iframe[scrolling=\"no\"]"
+        ],
+        "idsToHide": [
+            "player-container",
+            "top"
+        ],
+        "interval": 1600
+    },
+    "_config": {
+        "defaultInterval": 600
     }
 }
